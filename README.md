efcc0097e54843c5f1ab1c96bb0d92ce178e7a53
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2022-01-28 23:44:41 +0300

    docs: Moved URLs into README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # miio-proto
-MIIO protocol implementation for Rust
+Crate implements MIIO protocol which is used to control Xiaomi devices over WiFi/UDP.
+
+Some useful links:
+ * `https://github.com/marcelrv/XiaomiRobotVacuumProtocol/blob/master/Protocol.md` - Packet format
+ * `https://github.com/marcelrv/XiaomiRobotVacuumProtocol` - Xiaomi Vacuum cleaner JSON commands
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-//! Crate implements MIIO protocol which is used to control Xiaomi devices over WiFi/UDP.
-//!
-//! Some useful links:
-//! * `https://github.com/marcelrv/XiaomiRobotVacuumProtocol/blob/master/Protocol.md` - Packet format
-//! * `https://github.com/marcelrv/XiaomiRobotVacuumProtocol` - Xiaomi Vacuum cleaner JSON commands
-//!
 //! # Simple example
 //!
 //! ```no_run
